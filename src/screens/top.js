9ed1104562515191c84ee357d45e2662b31e1274
daleksprinter <g2119022@fun.ac.jp>
2020-05-29 19:45:21 +0900

    add components to Top

--- src/screens/top.js
+++ src/screens/top.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(24),
+      height: theme.spacing(24),
+    },
+  },
+}));
 
 const Top = () => {
-  return <div>topだよ</div>;
+
+	const classes = useStyles();
+
+	return (
+		<div className = {classes.root}>
+			<Paper evaluation = {0} >
+				<Button variant = "contained" color = "primary">
+					発行する
+				</Button>
+				<Typography variant = "h6" gutterBottom>発行ボタンを押してください</Typography>
+				<Typography variant = "h6" gutterBottom>
+					URL:
+				</Typography>
+			</Paper>
+		</div>
+	)
 };
 
 export default Top;
