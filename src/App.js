a5efc96a9698983ce7b3d646f09e4ca956cb160f
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-05-29 19:05:54 +0900

    3つの画面とrouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Top from "./screens/top";
+import Video from "./screens/video";
+import Profile from "./screens/profile";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <div className="mainFlex">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route exact path="/video" component={Video} />
+          <Route exact path="/profile" component={Profile} />
+          {/* <Route component={NotFound} /> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
