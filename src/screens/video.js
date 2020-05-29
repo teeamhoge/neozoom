880bb8bda1c53cf2765dece4607f3f6f17f2dd3f
daleksprinter <g2119022@fun.ac.jp>
2020-05-30 02:16:13 +0900

    [add]pass profile data to video component

--- src/screens/video.js
+++ src/screens/video.js
@@ -2,7 +2,22 @@ import React from "react";
 import Transcript from "./transcript"
 
 
-const Video = () => {
+const Video = (props) => {
+
+	const data = props.location.state;
+
+	/*
+	 * type definition of data
+	 * {
+	 *	room_id: string,
+	 *	nickname: string,
+	 *	sake: bool,
+	 *	tame: bool,
+	 * }
+	*/
+
+	//TODO: connect skyway with data.room_id
+
 	return (
 		<div>
 			<Transcript />
